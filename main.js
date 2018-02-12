7e2cf81fd9ee62f64d08d7bdda96f57c2c0c82f1
Bazzak <bazzak81@gmail.com>
2018-02-12 14:30:08 +0100

    dsmvlö

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-style();
-
-function style(){
-	if(localStorage.getItem("style") != null){ 												//Find stored color style in localStorage and overides it in HTML file.
-		document.querySelector("#style").setAttribute("href","style"+localStorage.getItem("style")+".css");
-	}else {
-		document.querySelector("#style").setAttribute("href","stylegrey.css");
-	}
-}
 var menuShow = false; 																	//Boolean to se if menu is showing or is hidden
-
-
+var list;									 											//Sets style dropdown to current style-name
+	
 window.onload = function(){																//Function that runs after page has loaded.
+	list = document.querySelector("select");
 	document.querySelector("select").addEventListener("change", changeStyle, false); 	//Adds listener to style dropdown	
-	var list = document.querySelector("select"); 										//Sets style dropdown to current style-name
 	list.value = localStorage.getItem("style");
 	document.querySelector("#menubutton").addEventListener("click", showMenu, false);
+	style();
 }
 
 
 function changeStyle(){																	//Function that runs when style dropdown has been changed by user
-	var list = document.querySelector("select");
 	document.querySelector("#style").setAttribute("href","style"+list.value+".css");
 	localStorage.setItem("style", list.value);											//Saves selected value in locakStorage
 }	
 
+function style(){
+	if(localStorage.getItem("style") != null){ 												//Find stored color style in localStorage and overides it in HTML file.
+		document.querySelector("#style").setAttribute("href","style"+localStorage.getItem("style")+".css");
+		list.value = localStorage.getItem("style");
+	}else {
+		document.querySelector("#style").setAttribute("href","stylegrey.css");
+		list.value = "grey";
+	}
+}
 
 function showMenu(){																	//Function that runs when menu button is pressed in mobile view
 	var menuItems = document.querySelectorAll(".menu");									//Saves all elements in nav (with class name "menu") in an array
