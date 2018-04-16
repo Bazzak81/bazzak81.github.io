1635067371f1e2e59b0443726f15955c66d45d6a
Jimmy Åkesson <bazzak81@gmail.com>
2018-04-16 20:08:56 +0200

    första sidan klar

--- main.js
+++ main.js
@@ -10,11 +10,27 @@ function showPosition(position) {
     JSON.stringify(position);
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-    alert("Latitude: " + lat);
+    console.log(parseInt(lat));
+    console.log(lon);
 
-    console.log(5 + 6);
+    getWeather(lat, lon);
 }
 
 function showError() {
     alert("Did not get a response");
+}
+
+function getWeather(lat, lon) {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + parseInt(lat) + "&lon=" + parseInt(lon) + "&APPID=92d45b077fa249614bfc79c61cf8b50f&units=metric",
+        dataType: "JSON"
+    }).done(function(data) {
+        document.getElementById("temp").innerHTML = data.main.temp;
+        document.getElementById("weather_image").src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        document.getElementById("weather").innerHTML = data.weather[0].description;
+        console.log(data.weather[0].icon);
+        console.log("Ajax-anrop klart, lyckades!");
+    }).fail(function(data) {
+        console.log("Ajax-anrop klart, misslyckat!");
+    });
 }
