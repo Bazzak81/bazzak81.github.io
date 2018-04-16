6c918ddfd91605fe4d78d33c99bf1d2c9901b969
Jimmy Åkesson <bazzak81@gmail.com>
2018-04-16 17:47:08 +0200

    funkar

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-console.log(5+6);
-
-$(document).ready(function(){
-    if(navigator.geolocation){
+$(document).ready(function() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else{
+    } else {
         alert("Din webbläsare stödjer inte geolocation");
     }
 });
 
-function showPosition(position){
-    alert(JSON.stringify(position));
+function showPosition(position) {
+    JSON.stringify(position);
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-    alert("Latitude: "+lat);
+    alert("Latitude: " + lat);
+
+    console.log(5 + 6);
+}
+
+function showError() {
+    alert("Did not get a response");
 }
