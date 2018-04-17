265cc5ca6bc900ed95106a3e34bbb7c0ad2dd9a3
Bazzak <bazzak81@gmail.com>
2018-04-17 19:05:06 +0200

    Massor

---
+++ findMovieScript.js
@@ -0,0 +1,36 @@
+function searchButton() {
+    console.log("Knappen tryckt");
+    var search = document.getElementById("searchString").value;
+    var searchString = search.split(' ').join('+');
+    console.log(searchString);
+
+    $.ajax({
+        url: "http://www.omdbapi.com/?apikey=bc059aff&s=" + searchString,
+        dataType: "JSON"
+    }).done(function(data) {
+        console.log(data.Search.length);
+        console.log(data.Search[0].Title);
+        var mediaData = '';
+        for (var i = 0; i < data.Search.length; i++) {
+            mediaData += '<div class="media">';
+            mediaData += '<div class="media-left media-middle">';
+            mediaData += '<a href="image">';
+            mediaData += '<img class="media-object" src="'
+            mediaData += data.Search[i].Poster;
+            mediaData += '" alt="image">';
+            mediaData += '</a>';
+            mediaData += '</div>';
+            mediaData += '<div class="media-body">';
+            mediaData += '<h6 class="media-heading">';
+            mediaData += data.Search[i].Title;
+            mediaData += '</h6>';
+            mediaData += '</div>';
+            mediaData += '</div>';
+            mediaData += '<hr>';
+        }
+        $('#resultList').html(mediaData);
+
+    }).fail(function(data) {
+
+    });
+}
