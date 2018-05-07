0ad2739d3e17288a19a4922f1089f49c2559d81c
Bazzak <bazzak81@gmail.com>
2018-05-07 14:58:23 +0200

    start link

--- saveFileScript.js
+++ saveFileScript.js
@@ -1,7 +1,6 @@
 $("#myForm").submit(function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    //test
 
     $.ajax({
         url: 'https://ddwap.mah.se/ag7416/server.php',
